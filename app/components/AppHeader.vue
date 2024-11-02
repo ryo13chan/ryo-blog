f572a8f5ca1907c5ca0e943e74ca2a8517c923d1
Narita Ryosuke <ryo13chan@gmail.com>
2024-11-02 16:21:18 +0900

    ヘッダー追加

---
+++ app/components/AppHeader.vue
@@ -0,0 +1,12 @@
+<template>
+  <header
+    class="w-full px-4 lg:px-8 py-2 border-b border-surface"
+  >
+    <NuxtLink to="/">
+      <h1 class="m-0">
+        Ryo Blog
+      </h1>
+    </NuxtLink>
+    <!-- TODO: カラー切り替え -->
+  </header>
+</template>
--- app/layouts/default.vue
+++ app/layouts/default.vue
@@ -3,18 +3,21 @@ const route = useRoute()
 </script>
 
 <template>
-  <div class="flex lg:justify-between gap-8 flex-col lg:flex-row">
-    <main class="flex-1 prose">
-      <slot />
-    </main>
-    <aside class="lg:w-80 sticky top-0 h-full">
-      <div class="flex flex-col gap-8">
-        <TableOfContents
-          v-if="route.name === 'slug'"
-          :path="route.path"
-        />
-        <TagList />
-      </div>
-    </aside>
+  <div>
+    <AppHeader />
+    <div class="flex lg:justify-between gap-8 flex-col lg:flex-row">
+      <main class="flex-1">
+        <slot />
+      </main>
+      <aside class="lg:w-80 sticky top-0 h-full">
+        <div class="flex flex-col gap-8">
+          <TableOfContents
+            v-if="route.name === 'slug'"
+            :path="route.path"
+          />
+          <TagList />
+        </div>
+      </aside>
+    </div>
   </div>
 </template>
