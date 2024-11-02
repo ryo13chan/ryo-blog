40a59f8fa08fecc54234be6f38b0a6a79abb3aab
Narita Ryosuke <ryo13chan@gmail.com>
2024-11-02 19:01:54 +0900

    見出し修正

--- app/components/TableOfContents.vue
+++ app/components/TableOfContents.vue
@@ -7,21 +7,23 @@ const path = defineModel<string>('/')
     <template #default="{ doc }">
       <div v-if="doc.body?.toc?.links?.length">
         <div class="text-xl mb-2 font-bold">
-          目次
+          Table of Contents
         </div>
         <!-- 見出し2 -->
         <div
           v-for="link in doc.body.toc.links"
           :key="link.id"
         >
-          <NuxtLink :href="`#${link.id}`">
-            {{ link.text }}
-          </NuxtLink>
+          <div class="my-2">
+            <NuxtLink :href="`#${link.id}`">
+              {{ link.text }}
+            </NuxtLink>
+          </div>
           <!-- 見出し3 -->
           <div
             v-for="childLink in link.children"
             :key="childLink.id"
-            class="pl-4"
+            class="pl-4 my-2"
           >
             <NuxtLink :href="`#${childLink.id}`">
               {{ childLink.text }}
