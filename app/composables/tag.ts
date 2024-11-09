b97b3a63de1c3bb0f3e25f40689cb5fc5ce7d6ce
Narita Ryosuke <ryo13chan@gmail.com>
2024-11-09 14:02:10 +0900

    タグ追加

--- app/composables/tag.ts
+++ app/composables/tag.ts
@@ -7,6 +7,18 @@ export const tags: Tag[] = [
   { key: 'javascript', label: 'JavaScript' },
   { key: 'vue', label: 'Vue.js' },
   { key: 'react', label: 'React' },
+  { key: 'markdown', label: 'Markdown' },
+  { key: 'openai', label: 'OpenAI' },
+  { key: 'nuxt', label: 'Nuxt.js' },
+  { key: '振り返り', label: '振り返り' },
+  { key: '2023', label: '2023年' },
+  { key: '2024', label: '2024年' },
+  { key: 'html', label: 'HTML' },
+  { key: 'css', label: 'CSS' },
+  { key: 'github', label: 'GitHub' },
+  { key: 'vscode', label: 'VSCode' },
+  { key: 'eslint', label: 'ESLint' },
+  { key: 'prettier', label: 'Prettier' },
 ]
 
 export const getTag = (key?: string): Tag | undefined => tags.find(tag => tag.key === key)
